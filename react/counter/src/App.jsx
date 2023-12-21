3b80d88915ec49482fe728c2d418c42770a2ea68
Prashant Panchal <prashantp2524@gmail.com>
2023-12-21 16:40:47 +0530

    react counter

---
+++ react/counter/src/App.jsx
@@ -0,0 +1,31 @@
+
+import './App.css'
+import { useState } from 'react'
+
+function App()
+{
+  const [value, setvalue] = useState(5)
+  const addValue = () =>
+  {
+    setvalue(value + 1)
+
+  }
+
+  const removeValue = () =>
+  {
+    setvalue(value - 1)
+  }
+
+  return (
+    <>
+      <h1>Counter</h1>
+      <h2>Counter value:{value}</h2>
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <button onClick={removeValue}>Remove Value</button>
+
+    </>
+  )
+}
+
+export default App
