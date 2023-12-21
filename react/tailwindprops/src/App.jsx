6dc03997fd539789f3f507f5472a61ce8728b295
Prashant Panchal <prashantp2524@gmail.com>
2023-12-21 18:26:52 +0530

    tailwindprops

---
+++ react/tailwindprops/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Card from './Card'
+
+function App()
+{
+
+  return (
+    <>
+      <h1 className='mb-5'>Tailwind CSS</h1>
+      <Card name={"prashant"} />
+      <Card />
+    </>
+  )
+}
+
+export default App
